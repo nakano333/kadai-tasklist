6008254bd52c34a5fa0bd9efec66361c9501ce36
nakano.nakano <takae_e@hotmail.co.jp>
2018-09-27 22:17:20 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,17 +1,15 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Task;
-import utils.DBUtil;
+import models.Message;
 
 /**
  * Servlet implementation class NewServlet
@@ -32,26 +30,11 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    em.getTransaction().begin();
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("message", new Message());
 
-	    Task m = new Task();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+    }
 
-	    String title = "taro";
-	    m.setTitle(title);
-
-	    String content = "hello";
-	    m.setContent(content);
-
-	    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-	    m.setCreated_at(currentTime);
-	    m.setUpdated_at(currentTime);
-
-	    em.persist(m);
-	    em.getTransaction().commit();
-
-	    response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-	    em.close();
-	}
 }
