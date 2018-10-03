830a03ffc11d9bb82026c9d588d5ffec6da0a023
nakano.nakano <takae_e@hotmail.co.jp>
2018-10-03 21:30:04 +0900

    Add EditServlet

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,5 +1,6 @@
 package controllers;
 
+
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
@@ -14,35 +15,35 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ShowServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/show")
-public class ShowServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ShowServlet() {
+    public EditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-
         Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
-
 
         em.close();
 
         request.setAttribute("message", m);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("message_id", m.getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
 
